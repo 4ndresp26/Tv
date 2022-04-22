545d13e4f1dedef38456246970ced6d9196e22d2
4ndresp26 <69642781+4ndresp26@users.noreply.github.com>
2022-04-21 22:19:37 -0500

    Add files via upload

---
+++ serie.ts
@@ -0,0 +1,5 @@
+export class Serie{
+    constructor(public id: number, public nombre:string, public Channel:String, public season: number,public desc:string, public link:string, public image:string){
+
+    }
+}
